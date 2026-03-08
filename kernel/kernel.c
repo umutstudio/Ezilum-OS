8dff3c7a4854b8fb7dd2b1867e2f17bd40d30ced
umutstudio <baharelidenk@gmail.com>
2026-03-08 16:17:46 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,1 +1,13 @@
+#include "kernel.h"
 
+void kernel_main() {
+    terminal_initialize();
+    terminal_write("MyOS kernel started\n");
+
+    memory_init();
+    scheduler_init();
+
+    while (1) {
+        scheduler_run();
+    }
+}
